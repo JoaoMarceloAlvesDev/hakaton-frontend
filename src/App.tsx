3e066dfd3590194f7d0b2f5992b484ed3236ad19
JoaoMarceloAlvesDev <joao333marcelo@gmail.com>
2026-07-04 19:36:55 -0300

    organizando-paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import SelecaoEscola from './pages/Home/selecao-escola'; // Ajuste o caminho se estiver dentro de uma pasta (ex: './components/selecao-escola')
+import Home from './pages/Home/index'; // Ajuste o caminho se estiver dentro de uma pasta (ex: './components/selecao-escola')
 
 export default function App() {
   return (
     <div>
-      <SelecaoEscola />
+      <Home />
     </div>
   );
 }
